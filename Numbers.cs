8cedee9430e3cecee3ec3117b66a763f8cae6b6b
Irfan476 <irfanandryanto47@students.amikom.ac.id>
2022-04-06 10:26:41 +0700

    File latihan overload method

---
+++ Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        private int _c, _d;
+        public int FindMinimum(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+
+        public int FindMinimum(int a, int b, float c)
+        {
+            _c = a < c ? a : (int)c;
+            _d = b < c ? b : (int)c;
+            return _c < _d ? _c : _d;
+        }
+
+        public int FindMaximum(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public int FindMaximum(int a, int b, float c)
+        {
+            _c = a > c ? a : (int)c;
+            _d = b > c ? b : (int)c;
+            return _c > _d ? _c : _d;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(10, 2));
+            Console.WriteLine("Minimum #2: {0}", number.FindMinimum(20, 54, 15));
+            Console.WriteLine();
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(98, 100));
+            Console.WriteLine("Maximum #2: {0}", number.FindMaximum(13, 64, 52));
+
+            Console.WriteLine("Tekan ENTER untuk melanjutkan");
+            Console.ReadKey();
+        }
+    }
+}
